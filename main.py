bc4287b56d850b91ce7f490d920de4c07512b013
Loh Jian Cong <lohjiancong@hotmail.com>
2022-06-15 16:25:30 +0800

    Simple Motion and Object Detection
    Simple motion detection using background subtraction (OpenCV) and objection detection using YOLOv3 (ImageAI).
    
    To-do:
    - Alerts
        - Vehicles detected + no motion > congestion alert
        - No vehicle detected + person detected + motion > intruder alert
    - Video stabilization
    - Pipeline for real-time video analysis and alert
    
    Limitations:
    - Compute: object detection extremely slow without GPU, tradeoff between speed, recall, and update frequency
    - Sensitivity to camera pose: motion detection algorithm relies on fixed camera pose, video stabilization required
    - Sensitivity to context: parameters for object detection might require tuning to work in different contexts
    - Assumptions require validation

---
+++ main.py
@@ -0,0 +1,100 @@
+"""
+References
+- https://docs.opencv.org/3.4/d1/dc5/tutorial_background_subtraction.html
+- https://github.com/OlafenwaMoses/ImageAI/blob/master/imageai/Detection/README.md
+
+YOLOv3 model download: https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5
+"""
+
+import cv2 as cv
+import numpy as np
+import argparse
+from imageai.Detection import ObjectDetection
+import os
+import _thread
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+################################ Parameters ################################
+# Motion detection
+BACK_SUB_THRESHOLD = 256
+FG_MASK_THRESHOLD = 0.0005
+# Object detection
+MIN_PERCENTAGE_PROB = 40
+DETECTION_SPEED = "fast" # Options: normal/fast/faster/fastest/flash
+
+# Subroutine for object detection executed on new thread
+def detect_objects(detector, custom, frame, results):
+    returned_image, detections = detector.detectObjectsFromImage(
+                                            input_type="array",
+                                            input_image=frame, 
+                                            output_type="array", 
+                                            minimum_percentage_probability=MIN_PERCENTAGE_PROB,
+                                            custom_objects = custom)
+    results["returned_image"] = returned_image
+    results["detections"] = detections
+
+def main():
+    parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
+                                                OpenCV. You can process both videos and images.')
+    parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='traffic.mp4')
+    args = parser.parse_args()
+
+    # Set up ImageAI object detector
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath( "yolo.h5")
+    detector.loadModel(detection_speed=DETECTION_SPEED)
+    custom = detector.CustomObjects(person=True, car=True, truck=True, motorcycle=True)
+    results = {}
+
+    # Set up OpenCV background subtractor
+    backSub = cv.createBackgroundSubtractorMOG2(varThreshold = BACK_SUB_THRESHOLD, detectShadows=False)
+    capture = cv.VideoCapture(cv.samples.findFileOrKeep(args.input))
+    if not capture.isOpened():
+        print('Unable to open: ' + args.input)
+        exit(0)
+
+    while True:
+        ret, frame = capture.read()
+        if frame is None:
+            break
+
+        # Preprocess frame 
+        frame = cv.resize(frame, (640,360))
+        processed_frame = cv.GaussianBlur(src=frame, ksize=(5,5), sigmaX=0)
+        fgMask = backSub.apply(processed_frame)
+        
+        # Threshold ratio of difference in entire image
+        diffRatio = np.count_nonzero(fgMask)/fgMask.size
+
+        # Display information
+        cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+        cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+                    cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+        cv.rectangle(frame, (10, 22), (200,42), (255,255,255), -1)
+        cv.putText(frame, "Motion detected: "+ str(diffRatio>FG_MASK_THRESHOLD), (15, 37),
+                    cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+        # Run object detection in new thread every 2 seconds
+        if capture.get(cv.CAP_PROP_POS_FRAMES) == 1:
+            returned_image, detections = detector.detectObjectsFromImage(
+                                            input_type="array",
+                                            input_image=frame, 
+                                            output_type="array", 
+                                            minimum_percentage_probability=50,
+                                            custom_objects = custom)
+            results["returned_image"] = returned_image
+            results["detections"] = detections
+        elif capture.get(cv.CAP_PROP_POS_FRAMES)%60 == 0:
+            _thread.start_new_thread(detect_objects, (detector, custom, frame, results,))
+
+        # Display frames 
+        cv.imshow('Frame', processed_frame)
+        cv.imshow('FG Mask', fgMask)
+        cv.imshow('Detection',results["returned_image"])
+        
+        keyboard = cv.waitKey(30)
+
+if __name__ == "__main__":
+    main()
