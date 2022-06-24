f5cdacb715a609f6d7075e475268e3489f39426b
Loh Jian Cong <lohjiancong@hotmail.com>
2022-06-24 11:48:29 +0800

    Remove stabilization and print congestion information

--- main.py
+++ main.py
@@ -1,128 +1,125 @@
 """
 References
-- https://github.com/abhiTronix/vidgear#videogear
 - https://docs.opencv.org/3.4/d1/dc5/tutorial_background_subtraction.html
 - https://github.com/OlafenwaMoses/ImageAI/blob/master/imageai/Detection/README.md
-
 YOLOv3 model download: https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5
 TinyYOLOv3 model download: https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo-tiny.h5
 """
 
-import os
 import argparse
 import cv2
-import numpy as np
 from imageai.Detection import ObjectDetection
-from vidgear.gears import VideoGear
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import numpy as np
+import _thread
 
 ################################ Parameters ################################
-FRAME_SIZE = (480,270)
-# Video stabilization
-options = {
-    'SMOOTHING_RADIUS': 250,
-    'BORDER_SIZE': 64,
-    'CROP_N_ZOOM': True
-}
+FRAME_SIZE = (640,360)
+HALF_FRAME_SIZE = (320,180)
+OUTPUT_SIZE = (640,540)
 # Motion detection
-GAUSSIAN_KERNEL = (11,11)
-GAUSSIAN_STDDEV = 10
+GAUSSIAN_KERNEL = (5,5)
+GAUSSIAN_STDDEV = 2
 BACK_SUB_THRESHOLD = 512
-FG_MASK_THRESHOLD = 0.0005
 # Object detection
 MIN_PERCENTAGE_PROB = 40
 DETECTION_SPEED = "normal" # Options: normal/fast/faster/fastest/flash
 ############################################################################
 
+def get_detection_area(detections):
+    area = 0
+    for object in detections:
+        x1, y1, x2, y2 = object["box_points"]
+        area += (x2-x1)*(y2-y1)
+    return area
+
+def detect_objects(detector, custom, motion_30frames, frame, results):
+    returned_image, detections = detector.detectObjectsFromImage(
+                                    input_type="array",
+                                    input_image=frame, 
+                                    output_type="array", 
+                                    minimum_percentage_probability=MIN_PERCENTAGE_PROB,
+                                    custom_objects = custom)
+    results["returned_image"] = returned_image
+    detection_area = get_detection_area(detections)
+    results['congestion_5s'].pop(0)
+    if detection_area != 0:
+        print(sum(motion_30frames)/len(motion_30frames)/detection_area)
+    if detection_area == 0 or sum(motion_30frames)/len(motion_30frames) > 0.1*detection_area:
+        results['congestion_5s'].append(0)
+    else:
+        results['congestion_5s'].append(1)
+
+
 def main():
     parser = argparse.ArgumentParser(description='This program combines video stabilization, motion detection, and object \
                                         detection to perform traffic video analysis.')
-    parser.add_argument('--input', type=str, help='Path to input video.', default='video/10.avi')
-    parser.add_argument('--output', type=str, help='Path to save output.', default='output/10.mp4')
-    parser.add_argument('--display', type=bool, help='Display analysis output.', default=False)
+    parser.add_argument('--input', type=str, help='Path to input video.', default='video/combined.mp4')
+    parser.add_argument('--output', type=str, help='Path to save output.', default='output/combined1.mp4')
     args = parser.parse_args()
 
-    # Open video stream with stabilization enabled
-    stream_stab = VideoGear(source=args.input, stabilize=True, **options).start()
-    stream_org = VideoGear(source=args.input).start()
-    frame_num = 0
+    # Set up OpenCV background subtractor
+    capture = cv2.VideoCapture(args.input)
+    backSub = cv2.createBackgroundSubtractorMOG2(varThreshold = BACK_SUB_THRESHOLD, detectShadows=False)
 
     # Set up ImageAI object detector
     detector = ObjectDetection()
     detector.setModelTypeAsTinyYOLOv3()
     detector.setModelPath( "model/yolo-tiny.h5")
     detector.loadModel(detection_speed=DETECTION_SPEED)
-    custom = detector.CustomObjects(person=True, car=True, truck=True, motorcycle=True)
+    custom = detector.CustomObjects(car=True, truck=True, motorcycle=True)
+    results = {}
+    motion_30frames = []
 
-    # Set up OpenCV background subtractor
-    backSub = cv2.createBackgroundSubtractorMOG2(varThreshold = BACK_SUB_THRESHOLD, detectShadows=False)
-    
+    # Save output
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(args.output, fourcc ,30,(960,540))
+    out = cv2.VideoWriter(args.output,fourcc, 30, OUTPUT_SIZE)
 
-    while True:
-        frame_num += 1
-        frame_processed = stream_stab.read()
-        if frame_processed is None:
-            break
-        frame_org = stream_org.read()
-        # Resize frame 
-        frame_stab = cv2.resize(frame_processed, FRAME_SIZE)
-        frame_org = cv2.resize(frame_org, FRAME_SIZE)
-        
-        # Motion detection: 
-        # 1. Apply Gaussian blur
-        # 2. Perform background subtraction 
-        # 3. Calculate ratio of difference in entire image
-        processed_frame = cv2.GaussianBlur(src=frame_stab, ksize=GAUSSIAN_KERNEL, sigmaX=GAUSSIAN_STDDEV)
-        fgMask = backSub.apply(processed_frame)
-        diffRatio = np.count_nonzero(fgMask)/fgMask.size     
-        
-        # Run object detection in new thread every 1/5 second
-        if (frame_num-1)%6 == 0:
-            print(frame_num)
-            returned_image, detections = detector.detectObjectsFromImage(
-                                            input_type="array",
-                                            input_image=frame_org, 
-                                            output_type="array", 
-                                            minimum_percentage_probability=MIN_PERCENTAGE_PROB,
-                                            custom_objects = custom)
-        
-        # Display frame information
-        fgMask = np.stack((fgMask,)*3, axis=-1)
-        cv2.rectangle(frame_org, (10, 2), (100,20), (255,255,255), -1)
-        cv2.putText(frame_org, str(frame_num), (15, 15),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-        cv2.putText(frame_org, "Original", (10, frame_org.shape[0]-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
-        cv2.putText(frame_stab, "Stabilized", (10, frame_stab.shape[0]-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
-        cv2.putText(fgMask, "Motion detected", (10, fgMask.shape[0]-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
-        cv2.putText(fgMask, str(diffRatio>0.001), (fgMask.shape[1]-100, fgMask.shape[0]-10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
-        cv2.putText(returned_image, "Object detected", (10, returned_image.shape[0]-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
-            
-        # Concatenate and ahow output window
-        top_row = np.concatenate((frame_org, frame_stab), axis=1)
-        bottom_row = np.concatenate((fgMask, returned_image), axis=1)
-        output_frame = np.concatenate((top_row, bottom_row), axis=0)
-        if args.display:
-            cv2.imshow("Traffic video analysis", output_frame)
-        out.write(output_frame)
-            
-        keyboard = cv2.waitKey(0)
-        if keyboard == ord("q"):
-            break
+    if not capture.isOpened():
+        print('Unable to open: ' + args.input)
+        exit(0)
     
-
-    cv2.destroyAllWindows()
-    out.release()
-    stream_stab.stop()
-    stream_org.stop()
+    while True:
+        ret, frame = capture.read()
+        if frame is None:
+            break
         
+        # Preprocessing and background subtraction
+        frame = cv2.resize(frame, FRAME_SIZE)
+        pp_frame = cv2.GaussianBlur(src=frame, ksize=GAUSSIAN_KERNEL, sigmaX=GAUSSIAN_STDDEV)
+        fgMask = backSub.apply(pp_frame)
+        
+        if capture.get(cv2.CAP_PROP_POS_FRAMES) == 1:
+            results["returned_image"] = frame.copy()
+            results["congestion_5s"] = [0]*5
+        motion_30frames.append(np.count_nonzero(fgMask))
+
+        # Run object detection in new thread every 30 frames
+        if (capture.get(cv2.CAP_PROP_POS_FRAMES)-1) % 30 == 0:
+            _thread.start_new_thread(detect_objects, (detector, custom, motion_30frames, frame, results))
+            motion_30frames = []
+
+        # Display information
+        cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+        cv2.putText(frame, str(capture.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+        cv2.putText(frame, "Congestion detected: "+str(sum(results['congestion_5s'])>=3), (10, fgMask.shape[0]-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
+        
+        # Write output frame to file
+        fgMask = np.stack((fgMask,)*3, axis=-1)
+        info = np.concatenate((cv2.resize(fgMask, HALF_FRAME_SIZE),cv2.resize(results["returned_image"], HALF_FRAME_SIZE)), axis=1)
+        output_frame = np.concatenate((frame, info), axis=0)
+        out.write(output_frame)
+
+        # cv2.imshow('Frame', frame)
+        # cv2.imshow('FG Mask', fgMask)
+        # cv2.imshow('Object', results["returned_image"])
+
+        # keyboard = cv2.waitKey(30)
+        # if keyboard == ord("q"):
+        #     break
+
 
 if __name__ == "__main__":
     main()
+
