8a4f45ab46db69aa9e66ff49102f3c02b807bc74
Loh Jian Cong <lohjiancong@hotmail.com>
2022-06-29 15:55:10 +0800

    Image textual analysis

--- main.py
+++ main.py
@@ -1,130 +1,92 @@
 """
-References
-- https://docs.opencv.org/3.4/d1/dc5/tutorial_background_subtraction.html
-- https://github.com/OlafenwaMoses/ImageAI/blob/master/imageai/Detection/README.md
-YOLOv3 model download: https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5
-TinyYOLOv3 model download: https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo-tiny.h5
+Implementation of Li and Dai, "Real-time Congestion Detection Based on Image Texture Analysis",
+https://doi.org/10.1016/j.proeng.2016.01.250
 """
 
-import argparse
 import cv2
-from imageai.Detection import ObjectDetection
 import numpy as np
-import _thread
+import argparse
+from skimage.feature import graycomatrix, graycoprops
+from skimage.exposure import rescale_intensity
+from skimage.measure import shannon_entropy
 
-################################ Parameters ################################
-FRAME_SIZE = (640,360)
-HALF_FRAME_SIZE = (320,180)
-OUTPUT_SIZE = (640,540)
-# Motion detection
-GAUSSIAN_KERNEL = (5,5)
-GAUSSIAN_STDDEV = 2
-BACK_SUB_THRESHOLD = 512
-# Object detection
-MIN_PERCENTAGE_PROB = 40
-DETECTION_SPEED = "normal" # Options: normal/fast/faster/fastest/flash
-############################################################################
-
-# Calculate total area of detected objects
-def get_detection_area(detections):
-    area = 0
-    for object in detections:
-        x1, y1, x2, y2 = object["box_points"]
-        area += (x2-x1)*(y2-y1)
-    return area
-
-# Determine if there is a potential congestion in the past second
-def detect_objects(detector, custom, motion_30frames, frame, results):
-    returned_image, detections = detector.detectObjectsFromImage(
-                                    input_type="array",
-                                    input_image=frame, 
-                                    output_type="array", 
-                                    minimum_percentage_probability=MIN_PERCENTAGE_PROB,
-                                    custom_objects = custom)
-    results["returned_image"] = returned_image
-    detection_area = get_detection_area(detections)
-    results['congestion_5s'].pop(0)
-    # Assume that there is a congestion if the mean amount of motion in 1 second is less than 
-    # 10% of area of detected objects
-    if detection_area == 0 or sum(motion_30frames)/len(motion_30frames) > 0.1*detection_area:
-        results['congestion_5s'].append(0)
-    else:
-        results['congestion_5s'].append(1)
-
+S_THRESHOLD = 6.5
 
 def main():
+
     parser = argparse.ArgumentParser(description='This program combines motion detection and object \
                                         detection to perform traffic video analysis.')
-    parser.add_argument('--input', type=str, help='Path to input video.', default='video/combined.mp4')
-    parser.add_argument('--output', type=str, help='Path to save output.', default='output/combined.mp4')
+    parser.add_argument('--input', type=str, help='Path to input video.', default='video/demo.mp4')
     args = parser.parse_args()
 
-    # Set up OpenCV background subtractor
     capture = cv2.VideoCapture(args.input)
-    backSub = cv2.createBackgroundSubtractorMOG2(varThreshold = BACK_SUB_THRESHOLD, detectShadows=False)
-
-    # Set up ImageAI object detector
-    detector = ObjectDetection()
-    detector.setModelTypeAsTinyYOLOv3()
-    detector.setModelPath( "model/yolo-tiny.h5")
-    detector.loadModel(detection_speed=DETECTION_SPEED)
-    custom = detector.CustomObjects(car=True, truck=True, motorcycle=True)
-    results = {}
-    motion_30frames = []
-
-    # Save output
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(args.output,fourcc, 30, OUTPUT_SIZE)
+    bbox = None
+    hist = [0]*30
 
     if not capture.isOpened():
         print('Unable to open: ' + args.input)
         exit(0)
-    
+
     while True:
-        ret, frame = capture.read()
+        _, frame = capture.read()
         if frame is None:
             break
         
-        # Preprocessing and background subtraction
-        frame = cv2.resize(frame, FRAME_SIZE)
-        pp_frame = cv2.GaussianBlur(src=frame, ksize=GAUSSIAN_KERNEL, sigmaX=GAUSSIAN_STDDEV)
-        fgMask = backSub.apply(pp_frame)
+        frame = cv2.resize(frame, (960,640))
+
+        # Allow user to select region of interest
+        if bbox == None:
+            cv2.rectangle(frame, (330, 0), (960, 30), color=(255,255,255), thickness=-1)
+            cv2.putText(frame, "Select region of interest and press Enter. Press \"c\" to cancel.", (frame.shape[0]-300, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,0,0), 1)
+            bbox = cv2.selectROI("Select Region of Interest",frame, False)
+            if bbox == (0,0,0,0):
+                exit(0)
+            cv2.destroyWindow("Select Region of Interest")
+
+        # Preprocessing
+        # 1. Crop to region of interest
+        # 2. Convert to grayscale
+        # 3. Reduce pixel intensity from 256-level to 32-level
+        processed = frame[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
+        processed = cv2.cvtColor(processed, cv2.COLOR_BGR2GRAY)
+        processed = rescale_intensity(processed, in_range=(0,255), out_range=(0, 31)).astype(np.int8)
+
+        # Calculate GLCM, entropy S_p, and energy feature S'_g
+        glcm = graycomatrix(processed, distances=[1], 
+                            angles=[0,np.pi/4, np.pi/2, np.pi*3/4], levels=32)
+        energy = -np.log(graycoprops(glcm,prop='ASM')[0])
+        entropy = np.zeros(4)
+        for i in range(4):
+            entropy[i] = shannon_entropy(glcm[:,:,:,i])
         
-        if capture.get(cv2.CAP_PROP_POS_FRAMES) == 1:
-            results["returned_image"] = frame.copy()
-            results["congestion_5s"] = [0]*5
-        motion_30frames.append(np.count_nonzero(fgMask))
+        S = sum(energy)/4 + sum(entropy)/4
 
-        # Run object detection in new thread every 30 frames
-        if (capture.get(cv2.CAP_PROP_POS_FRAMES)-1) % 30 == 0:
-            _thread.start_new_thread(detect_objects, (detector, custom, motion_30frames, frame, results))
-            motion_30frames = []
-
-        # Display information
-        cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
-        cv2.putText(frame, str(capture.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-        # Assume that there is a congestion if >= 3 of the last 5 seconds has little motion relative to
-        # area of vehicles 
-        cv2.putText(frame, "Congestion detected: "+str(sum(results['congestion_5s'])>=3), (10, fgMask.shape[0]-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
+        # Record S values in last 30 frames       
+        hist.pop(0)
+        hist.append(S)
         
-        # Write output frame to file
-        fgMask = np.stack((fgMask,)*3, axis=-1)
-        info = np.concatenate((cv2.resize(fgMask, HALF_FRAME_SIZE),cv2.resize(results["returned_image"], 
-                    HALF_FRAME_SIZE)), axis=1)
-        output_frame = np.concatenate((frame, info), axis=0)
-        out.write(output_frame)
+        # Congestion detected if mean S is greater than threshold
+        if sum(hist)/30 >= S_THRESHOLD:
+            cv2.putText(frame, "Congestion detected", (10, frame.shape[0]-10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,0,255), 2)
+        else:
+            cv2.putText(frame, "No congestion detected", (10, frame.shape[0]-10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,255,0), 2)
 
-        # cv2.imshow('Frame', frame)
-        # cv2.imshow('FG Mask', fgMask)
-        # cv2.imshow('Object', results["returned_image"])
+        cv2.rectangle(frame, (330, 0), (960, 30), color=(255,255,255), thickness=-1)
+        cv2.putText(frame, "Press \"s\" to select a new region of interest. Press \"c\" to exit.", (frame.shape[0]-300, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6 , (0,0,0), 1)
+        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), color=(255,0,0), thickness=2)
 
-        # keyboard = cv2.waitKey(30)
-        # if keyboard == ord("q"):
-        #     break
-
+        # Display frame
+        cv2.imshow('Video', frame)
+        keyboard = cv2.waitKey(30)
+        if keyboard == ord("c"):
+            break
+        if keyboard == ord("s"):
+            cv2.destroyWindow("Video")
+            bbox = None
 
 if __name__ == "__main__":
     main()
-
